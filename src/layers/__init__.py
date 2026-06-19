d42b7d1aa713ad6620d698b61ee8369ffdc657fc
marcdaghar <marc.gilbert.daghar@gmail.com>
2026-06-19 13:47:42 +0200

    __init__.py

---
+++ src/layers/__init__.py
@@ -0,0 +1,9 @@
+# src/layers/__init__.py
+"""
+Layer modules for ICES v2 architecture.
+"""
+
+from .agent_physics import AgentPhysics
+from .economic_mechanisms import EconomicMechanisms
+from .institutions import InstitutionalLayer
+from .metrics import MetricsLayer
